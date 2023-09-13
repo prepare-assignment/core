e056c10968fcc1577c1c4cfa221ca71bee4890cf
Bonajo <m.bonajo@fontys.nl>
2023-09-13 16:01:11 +0200

    fix: test xdg_home only on linux

--- tests/utils/test_cache.py
+++ tests/utils/test_cache.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from pathlib import Path
 
 import pytest
@@ -6,11 +7,15 @@ import pytest
 from prepare.utils import get_cache_path
 
 
-def test_cache() -> None:
+def test_cache_xdg_home() -> None:
     old_cache_home = os.environ.get("XDG_CACHE_HOME", None)
     expected = Path("test/prepare")
     os.environ["XDG_CACHE_HOME"] = "test"
     cache_path = get_cache_path()
     if old_cache_home is not None:
         os.environ["XDG_CACHE_HOME"] = old_cache_home
-    assert cache_path == expected
+    if sys.platform == "linux":
+        same = cache_path == expected
+    else:
+        same = True
+    assert same
