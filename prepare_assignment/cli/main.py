a4b3f954c8cc1862671f350178a00b84d9729434
Bonajo <m.bonajo@fontys.nl>
2025-03-14 10:34:04 +0100

    feat: display info about prepare

--- prepare_assignment/cli/main.py
+++ prepare_assignment/cli/main.py
@@ -1,17 +1,34 @@
+import os.path
+import sys
 from typing import Optional
 
 import typer
 from typing_extensions import Annotated
 
+from prepare_assignment import __version__
 from prepare_assignment.cli.task import app as task_app
 from prepare_assignment.cli.tasks import app as tasks_app
 from prepare_assignment.core.main import prepare
 from prepare_assignment.data.config import GitMode
 from prepare_assignment.data.constants import CONFIG
+from prepare_assignment.utils.paths import get_config_path
+from prepare_assignment.utils.virtual_env import get_virtualenv_name
 
 app = typer.Typer(invoke_without_command=False)
 app.add_typer(task_app, name="task")
 app.add_typer(tasks_app, name="tasks")
+
+
+@app.callback(invoke_without_command=True)
+def main(ctx: typer.Context):
+    if ctx.invoked_subcommand is None:
+        config_path = os.path.join(get_config_path(), 'config.yml')
+        config_exists = os.path.exists(config_path)
+        typer.echo(f"Prepare version: {__version__}")
+        typer.echo(f"Config in use: {config_exists}")
+        typer.echo(f"Config path: {config_path}")
+        typer.echo(f"Python version: {sys.version.split(" ")[0]}")
+        typer.echo(f"Virtual env: {get_virtualenv_name()}")
 
 
 @app.command()
